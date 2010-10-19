13201ab842a7e69fbf4b655c55beb1d863874fb2
Martin Pecka <peci1@seznam.cz>
2010-10-19 15:46:20 +0200

    Completed the desired ConfigurationManager functionality.

--- src/cz/cuni/mff/peckam/java/origamist/configuration/ConfigurationManagerImpl.java
+++ src/cz/cuni/mff/peckam/java/origamist/configuration/ConfigurationManagerImpl.java
@@ -3,7 +3,12 @@
  */
 package cz.cuni.mff.peckam.java.origamist.configuration;
 
+import java.util.Locale;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
 import cz.cuni.mff.peckam.java.origamist.services.ConfigurationManager;
+import cz.cuni.mff.peckam.java.origamist.utils.LocaleConverter;
 
 /**
  * A default implementation of a configuration manager that stores the configuration in Preferences.
@@ -13,21 +18,64 @@ import cz.cuni.mff.peckam.java.origamist.services.ConfigurationManager;
 public class ConfigurationManagerImpl implements ConfigurationManager
 {
 
+    /** The active configuration. */
+    protected Configuration configuration = null;
+
+    /** The preferences node associated with this configuration manager. */
+    protected Preferences   prefs         = null;
+
     /**
-     * The active configuration.
+     * Load the configuration from the preferences. If the configuration is already loaded, this reloads the values from
+     * the properties into the active configuration (and will fire appropriate <code>PropertyChangeEvent</code>s).
      */
-    protected Configuration configuration = new Configuration();
+    public void load()
+    {
+        if (configuration == null)
+            configuration = new Configuration();
+
+        Locale locale = null;
+        Locale diagramLocale = null;
+
+        try {
+            prefs = Preferences.userNodeForPackage(ConfigurationManager.class);
+            locale = LocaleConverter.parse(prefs.get("locale", null));
+            diagramLocale = LocaleConverter.parse(prefs.get("diagramLocale", null));
+        } catch (SecurityException e) {
+            System.err
+                    .println("Couldn't load configuration because of security constraints. Using default configuration.");
+            // the default configuration will be loaded further, because all values will remain null
+        }
+
+        if (locale == null)
+            locale = Locale.getDefault();
+        configuration.setLocale(locale);
+
+        if (diagramLocale == null)
+            diagramLocale = Locale.getDefault();
+        configuration.setDiagramLocale(diagramLocale);
+    }
 
     @Override
     public Configuration get()
     {
+        if (configuration == null)
+            load();
         return configuration;
     }
 
     @Override
-    public void persist()
+    public void persist() throws BackingStoreException
     {
-        // TODO save configuration to preferences
+        if (prefs == null || configuration == null)
+            return;
+
+        String tmp = LocaleConverter.print(configuration.getLocale());
+        prefs.put("locale", tmp == null ? "" : tmp);
+
+        tmp = LocaleConverter.print(configuration.getDiagramLocale());
+        prefs.put("diagramLocale", tmp == null ? "" : tmp);
+
+        prefs.flush();
     }
 
 }
--- src/cz/cuni/mff/peckam/java/origamist/services/ConfigurationManager.java
+++ src/cz/cuni/mff/peckam/java/origamist/services/ConfigurationManager.java
@@ -3,6 +3,8 @@
  */
 package cz.cuni.mff.peckam.java.origamist.services;
 
+import java.util.prefs.BackingStoreException;
+
 import cz.cuni.mff.peckam.java.origamist.configuration.Configuration;
 
 /**
@@ -13,13 +15,22 @@ import cz.cuni.mff.peckam.java.origamist.configuration.Configuration;
 public interface ConfigurationManager
 {
     /**
+     * Loads the values of the configuration. May be used if you don't want the first call to <code>get()</code> to be
+     * slow. If this is called when a configuration exists, this will overwrite the active configuration values by the
+     * loaded ones (and will trigger appropriate <code>PropertyChangeEvent</code>s).
+     */
+    void load();
+
+    /**
      * @return The active configuration of the program.
      */
     Configuration get();
 
     /**
-     * Persist the current configuration.
+     * Persist the current configuration. If the configuration has not been loaded yet, does nothing.
+     * 
+     * @throws BackingStoreException If the saving failed.
      */
-    void persist();
+    void persist() throws BackingStoreException;
 
 }
