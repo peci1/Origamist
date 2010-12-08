a45f8f09f4c42c2622938bb0d1e70f396c764def
Martin Pecka <peci1@seznam.cz>
2010-12-09 00:25:53 +0100

    Made LocalizedString more effective using a static cache for resource bundles.

--- src/cz/cuni/mff/peckam/java/origamist/utils/LocalizedString.java
+++ src/cz/cuni/mff/peckam/java/origamist/utils/LocalizedString.java
@@ -5,6 +5,7 @@ package cz.cuni.mff.peckam.java.origamist.utils;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Hashtable;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -21,11 +22,28 @@ public class LocalizedString
 {
 
     /** The key to search for in the resource bundle. */
-    protected String key;
+    protected String                                 key;
     /** The cached text to display. */
-    protected String text;
+    protected String                                 text;
     /** The name of the resource bundle to use. */
-    protected String bundleName;
+    protected String                                 bundleName;
+    /** The cache of resource bundles for the active locale. */
+    private static Hashtable<String, ResourceBundle> bundles = new Hashtable<String, ResourceBundle>();
+    /** The cached application locale. */
+    private static Locale                            locale  = ServiceLocator.get(ConfigurationManager.class).get()
+                                                                     .getLocale();
+
+    static {
+        ServiceLocator.get(ConfigurationManager.class).get()
+                .addPropertyChangeListener("locale", new PropertyChangeListener() {
+                    @Override
+                    public void propertyChange(PropertyChangeEvent evt)
+                    {
+                        bundles.clear();
+                        locale = (Locale) evt.getNewValue();
+                    }
+                });
+    }
 
     /**
      * Create a localized string by reading <code>key</code> from a resource bundle with name <code>name</code>.
@@ -47,12 +65,22 @@ public class LocalizedString
      */
     protected void updateText()
     {
-        ResourceBundle bundle = ResourceBundle.getBundle(bundleName, getLocale());
         try {
-            text = bundle.getString(key);
+            text = getBundle().getString(key);
         } catch (MissingResourceException e) {
             text = "";
         }
+    }
+
+    /**
+     * @return The resource bundle associated with this localized string in the current locale.
+     */
+    protected ResourceBundle getBundle()
+    {
+        if (bundles.get(bundleName) == null) {
+            bundles.put(bundleName, ResourceBundle.getBundle(bundleName, locale));
+        }
+        return bundles.get(bundleName);
     }
 
     /**
@@ -65,7 +93,7 @@ public class LocalizedString
      */
     protected Locale getLocale()
     {
-        return ServiceLocator.get(ConfigurationManager.class).get().getLocale();
+        return locale;
     }
 
     /**
