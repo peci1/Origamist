94ffaafe624aa70e336c2451cb54137a458d84ba
Martin Pecka <peci1@seznam.cz>
2010-12-14 22:24:54 +0100

    Added the first "touch" of the Editor. Also added some Ant tasks - they mainly serve for launching the applets as JNLP applications and for launching the editor. The .jnlp files are generated from now on. It is due to the codebase attribute of the <jnlp> element. It was declared optional in 6u18, but has been redeclared as required in 6u20. The recent JRE (6u23) still has it declared as required. But it would be bad to have a really hardcoded string there. So the .jnlp files were removed from the repository and they are created by Ant. The default codebase is the directory containing build.xml, but it can be set in the build.xml to another URL.

---
+++ src/cz/cuni/mff/peckam/java/origamist/gui/editor/OrigamiEditor.java
@@ -0,0 +1,154 @@
+/**
+ * 
+ */
+package cz.cuni.mff.peckam.java.origamist.gui.editor;
+
+import java.applet.AppletContext;
+import java.awt.Container;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.swing.JApplet;
+import javax.swing.JRootPane;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import cz.cuni.mff.peckam.java.origamist.gui.CommonGui;
+import cz.cuni.mff.peckam.java.origamist.logging.GUIAppender;
+import cz.cuni.mff.peckam.java.origamist.services.ServiceLocator;
+import cz.cuni.mff.peckam.java.origamist.services.interfaces.ConfigurationManager;
+
+/**
+ * The editor of the origami model. <br />
+ * <br />
+ * Properties handled by this class (events are fired when they change - but not at startup):
+ * <ul>
+ * </ul>
+ * 
+ * This applet is intended to be started using a bootstrapper that will add support for Java3D without the need to
+ * install it on the client computer.
+ * 
+ * @author Martin Pecka
+ */
+public class OrigamiEditor extends CommonGui
+{
+    private static final long serialVersionUID = -6853141518719373854L;
+
+    /** The bootstrapper that has started this applet, or <code>null</code>, if it has not been bootstrapped. */
+    protected JApplet         bootstrap        = null;
+
+    /**
+     * Instantiate the origami viewer without a bootstrapper.
+     */
+    public OrigamiEditor()
+    {
+        this(null);
+    }
+
+    /**
+     * Instanitate the origami viewer with the given bootstrapper.
+     * 
+     * @param bootstrap The bootstrapper that starts this applet.
+     */
+    public OrigamiEditor(JApplet bootstrap)
+    {
+        super();
+        this.bootstrap = bootstrap;
+    }
+
+    /**
+     * Create and setup all the form components.
+     */
+    @Override
+    protected void createComponents()
+    {
+
+    }
+
+    /**
+     * Setup the form layout.
+     */
+    @Override
+    protected void buildLayout()
+    {
+
+    }
+
+    @Override
+    protected void setupLoggers()
+    {
+        super.setupLoggers();
+
+        Logger l = Logger.getLogger("editor");
+        l.setResourceBundle(ResourceBundle.getBundle("editor", ServiceLocator.get(ConfigurationManager.class).get()
+                .getLocale()));
+        l.setLevel(Level.ALL);
+        l.addAppender(new GUIAppender(this));
+    }
+
+    @Override
+    protected void registerServices()
+    {
+        super.registerServices();
+        ServiceLocator.add(OrigamiEditor.class, this);
+    }
+
+    // bootstrapping support
+
+    @Override
+    public JRootPane getRootPane()
+    {
+        if (bootstrap != null)
+            return bootstrap.getRootPane();
+        return super.getRootPane();
+    }
+
+    @Override
+    public Container getContentPane()
+    {
+        if (bootstrap != null)
+            return bootstrap.getContentPane();
+        return super.getContentPane();
+    }
+
+    @Override
+    public URL getDocumentBase()
+    {
+        if (bootstrap != null)
+            return bootstrap.getDocumentBase();
+        return super.getDocumentBase();
+    }
+
+    @Override
+    public URL getCodeBase()
+    {
+        if (bootstrap != null)
+            return bootstrap.getCodeBase();
+        return super.getCodeBase();
+    }
+
+    @Override
+    public String getParameter(String name)
+    {
+        if (bootstrap != null)
+            return bootstrap.getParameter(name);
+        return super.getParameter(name);
+    }
+
+    @Override
+    public AppletContext getAppletContext()
+    {
+        if (bootstrap != null)
+            return bootstrap.getAppletContext();
+        return super.getAppletContext();
+    }
+
+    @Override
+    public String getAppletInfo()
+    {
+        if (bootstrap != null)
+            return bootstrap.getAppletInfo();
+        return super.getAppletInfo();
+    }
+}
---
+++ src/cz/cuni/mff/peckam/java/origamist/gui/editor/OrigamiEditorBootstrapper.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package cz.cuni.mff.peckam.java.origamist.gui.editor;
+
+import cz.cuni.mff.peckam.java.origamist.gui.Java3DBootstrappingApplet;
+
+/**
+ * This applet just runs OrigamiEditor and adds support for Java3D native libraries.
+ * 
+ * @author Martin Pecka
+ */
+public class OrigamiEditorBootstrapper extends Java3DBootstrappingApplet
+{
+
+    /** */
+    private static final long serialVersionUID = 859355750244428460L;
+
+    @Override
+    protected String getApplicationClassName()
+    {
+        return "cz.cuni.mff.peckam.java.origamist.gui.editor.OrigamiEditor";
+    }
+
+    @Override
+    protected String[] getApplicationRootPackages()
+    {
+        return new String[] { "cz.cuni.mff.peckam.java.origamist", "org.w3c._2001.xmlschema", "javax.swing.origamist" };
+    }
+
+}
